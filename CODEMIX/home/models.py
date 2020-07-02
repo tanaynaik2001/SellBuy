632c9024ed96a154aa5a8fc0dc9c41de21bf341b
Shubh Nisar <65038837+Shubh-Nisar@users.noreply.github.com>
2020-07-02 19:16:56 +0530

    Added Price List via Admin Panel

--- CODEMIX/home/models.py
+++ CODEMIX/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Crop(models.Model):
+    cropName = models.CharField(max_length=100)
+    cropPrice = models.IntegerField()
+
+    def __str__(self): 
+        return self.cropName
--- CODEMIX/home/views.py
+++ CODEMIX/home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Crop
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def price(request):
+    crop = Crop.objects.all()
+
+    return render(request, 'price.html',{'crop':crop})
